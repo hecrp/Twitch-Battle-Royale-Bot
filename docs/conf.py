4396ca8d4f089fecc73817c84c943af475d8cfab
Héctor Rodríguez Pérez <alu0100774429@ull.edu.es>
2024-08-27 14:57:27 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../ttv_battleroyale'))
 
 project = 'Battle Royale Twitch Chat Bot'
 copyright = '2024, Héctor Rodríguez (hecrp)'
@@ -19,3 +20,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Add any additional configuration to resolve import issues
+autodoc_mock_imports = ['twitchio']
